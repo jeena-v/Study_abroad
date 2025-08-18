1af5cb96324dedb51b76dd0d06533079b272ae60
Jeena V <jeenav.valsan@gmail.com>
2025-08-18 16:09:53 +0400

    Updated settings

--- study_abroad/settings.py
+++ study_abroad/settings.py
@@ -120,17 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
-# settings.py
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- This must exist
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
